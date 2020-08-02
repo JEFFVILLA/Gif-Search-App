381ecbf77fea0b93441ee07e5597f11fd4324f0c
jeffvilla <csjeffri1994@gmail.com>
2020-08-03 01:05:43 +0200

    Docs Add

---
+++ docs/precache-manifest.c6138d4e36551fe3134d0171aac4b290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01606eb2f226ace9b1c68bd3775709dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b2e637190450e41e8e",
+    "url": "/static/css/main.c39523ae.chunk.css"
+  },
+  {
+    "revision": "fc8297614e4f448f12ef",
+    "url": "/static/js/2.969b929d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.969b929d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b2e637190450e41e8e",
+    "url": "/static/js/main.3c238f63.chunk.js"
+  },
+  {
+    "revision": "ded719702d9692d2c54c",
+    "url": "/static/js/runtime-main.6ecacd89.js"
+  }
+]);
